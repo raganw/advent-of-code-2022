8c10ad66de37d3d59424f673948cf313d9e3004a
Ragan Webber <raganw@gmail.com>
2022-12-06 22:45:08 -0800

    day 7 - start

---
+++ src/bin/07.rs
@@ -0,0 +1,33 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines: Vec<&str> = input.lines().collect();
+    dbg!(&lines);
+
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 7);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_one(&input), Some(95437));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 7);
+        assert_eq!(part_two(&input), None);
+    }
+}
