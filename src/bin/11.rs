f1aaddc8ca945dc5a266ce78ad952c56b76ae89c
Ragan Webber <raganw@gmail.com>
2022-12-11 20:49:48 -0800

    day 11 refactor

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -2,30 +2,27 @@ use itertools::Itertools;
 use std::{collections::VecDeque, cell::RefCell};
 
 use nom::{
-    bytes::complete::{tag, take_until1},
+    branch::alt,
+    bytes::complete::tag,
     character::complete::newline,
     multi::separated_list1,
-    sequence::delimited,
+    sequence::{delimited, preceded},
     IResult,
 };
 
 #[derive(Debug)]
-struct Operation<'a>(&'a str);
+enum Operation {
+    Mult(u64),
+    Add(u64),
+    Square,
+}
 
-impl <'a> Operation<'a> {
+impl Operation {
     fn perform_operation(&self, old: u64) -> u64 {
-        match self.0 {
-            "old * 19" => old * 19,
-            "old + 6" => old + 6,
-            "old * old" => old * old,
-            "old + 3" => old + 3,
-            "old * 2" => old * 2,
-            "old + 2" => old + 2,
-            "old * 11" => old * 11,
-            "old + 7" => old + 7,
-            "old + 1" => old + 1,
-            "old + 5" => old + 5,
-            _ => unimplemented!("The operation '{}' is unimplemented", self.0),
+        match self {
+            Operation::Square => old * old,
+            Operation::Add(value) => old + value,
+            Operation::Mult(value) => old * value,
         }
     }
 }
@@ -40,19 +37,38 @@ impl Test {
 }
 
 #[derive(Debug)]
-struct Monkey<'a> {
+struct Monkey {
     items: RefCell<VecDeque<u64>>,
-    operation: Operation<'a>,
+    operation: Operation,
     test: Test,
     true_target: usize,
     false_target: usize,
     inspection_count: u64,
 }
 
+/// Operation: new = old * 2
+fn op_square(input: &str) -> IResult<&str, Operation> {
+    let (input, _) = tag("new = old * old")(input)?;
+    Ok((input, Operation::Square))
+}
+
+fn op_add(input: &str) -> IResult<&str, Operation> {
+    let (input, value) = preceded(tag("new = old + "), nom::character::complete::u64)(input)?;
+    Ok((input, Operation::Add(value)))
+}
+fn op_mult(input: &str) -> IResult<&str, Operation> {
+    let (input, value) = preceded(tag("new = old * "), nom::character::complete::u64)(input)?;
+    Ok((input, Operation::Mult(value)))
+}
+fn operation(input: &str) -> IResult<&str, Operation> {
+    let (input, operation) = alt((op_square, op_add, op_mult))(input)?;
+    Ok((input, operation))
+}
+
 fn monkey(input: &str) -> IResult<&str, Monkey> {
     let (input, _) = delimited(tag("Monkey "), nom::character::complete::u32, tag(":\n"))(input)?;
     let (input, items) = delimited(tag("  Starting items: "), separated_list1(tag(", "), nom::character::complete::u64), newline)(input)?;
-    let (input, operation) = delimited(tag("  Operation: new = "), take_until1("\n"), newline)(input)?;
+    let (input, operation) = delimited(tag("  Operation: "), operation, newline)(input)?;
     let (input, test) = delimited(tag("  Test: divisible by "), nom::character::complete::u64, newline)(input)?;
     let (input, true_target) = delimited(tag("    If true: throw to monkey "), nom::character::complete::u32, newline)(input)?;
     let (input, false_target) = delimited(tag("    If false: throw to monkey "), nom::character::complete::u32, newline)(input)?;
@@ -62,7 +78,7 @@ fn monkey(input: &str) -> IResult<&str, Monkey> {
     let monkey = Monkey {
         items,
         test: Test(test),
-        operation: Operation(operation),
+        operation,
         true_target: true_target as usize,
         false_target: false_target as usize,
         inspection_count: 0,
@@ -89,17 +105,14 @@ fn lcm(a: u64, b: u64) -> u64 {
     (a * b) / gcd(a, b)
 }
 
-/// Part One
-pub fn part_one(input: &str) -> Option<u64> {
-    let (_, mut monkeys) = parse_input(input).unwrap();
-
-    for _ in 0..20 {
+fn monkey_business<F: Fn(u64) -> u64>(mut monkeys: Vec<Monkey>, rounds: u32, scaling_operation: F) -> u64 {
+    for _ in 0..rounds {
         for monkey_idx in 0..monkeys.len() {
             let mut target_monkey_and_items: Vec<(usize, u64)> = Vec::new();
             if let Some(monkey) = monkeys.get_mut(monkey_idx) {
                 while let Some(item) = monkey.items.borrow_mut().pop_front() {
                     monkey.inspection_count += 1;
-                    let new_value = monkey.operation.perform_operation(item) / 3;
+                    let new_value = scaling_operation(monkey.operation.perform_operation(item));
                     let target = if monkey.test.test(new_value) {
                         monkey.true_target
                     } else {
@@ -119,44 +132,22 @@ pub fn part_one(input: &str) -> Option<u64> {
     let mut result: Vec<u64> = monkeys.iter().map(|m| m.inspection_count).collect();
     result.sort();
     let (a, b) = result.iter().rev().take(2).collect_tuple().unwrap();
-    let result = a * b;
+     a * b
+}
+
+/// Part One
+pub fn part_one(input: &str) -> Option<u64> {
+    let (_, monkeys) = parse_input(input).unwrap();
+    let result = monkey_business(monkeys, 20, |val| val / 3);
 
     Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let (_, mut monkeys) = parse_input(input).unwrap();
+    let (_, monkeys) = parse_input(input).unwrap();
     let scaling_factor = monkeys.iter().fold(1, |acc, monkey| lcm(acc, monkey.test.0));
 
-    for _ in 0..10_000 {
-        for monkey_idx in 0..monkeys.len() {
-            let mut target_monkey_and_items: Vec<(usize, u64)> = Vec::new();
-            if let Some(monkey) = monkeys.get_mut(monkey_idx) {
-                while let Some(item) = monkey.items.borrow_mut().pop_front() {
-                    monkey.inspection_count += 1;
-                    let new_value = monkey.operation.perform_operation(item);
-                    let new_value = new_value % scaling_factor;
-                    let target = if monkey.test.test(new_value) {
-                        monkey.true_target
-                    } else {
-                        monkey.false_target
-                    };
-                    target_monkey_and_items.push((target, new_value));
-                }
-            }
-            for (target, value) in target_monkey_and_items {
-                if let Some(monkey) = monkeys.get_mut(target) {
-                    monkey.items.borrow_mut().push_back(value);
-                }
-            }
-        }
-    }
-
-    let mut result: Vec<u64> = monkeys.iter().map(|m| m.inspection_count).collect();
-    result.sort();
-    let (a, b) = result.iter().rev().take(2).collect_tuple().unwrap();
-    let result = a * b;
-
+    let result = monkey_business(monkeys, 10_000, |val| val % scaling_factor);
     Some(result)
 }
 
